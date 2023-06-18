38ad5a0b585ccedee71cbfd3d2228f65ceb7da27
sagarkumar2001 <121309302+sagarkumar2001@users.noreply.github.com>
2023-06-18 16:58:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+alert("Welcome to Drum Kit !!!!!!!!");
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    var but=this.innerHTML;
+    makesound(but);
+    animation(but);
+  
+} );}
+document.addEventListener("keypress",function(sagar){
+       makesound(sagar.key);
+       animation(sagar.key);
+})
+function makesound(keys){
+       switch(keys){
+              case "w":var tom1=new Audio("sounds/tom-1.mp3");
+                     tom1.play();break;
+              case "a":var tom2=new Audio("sounds/tom-2.mp3");
+                     tom2.play();break;
+               case "s":var tom3=new Audio("sounds/tom-3.mp3");
+                     tom3.play();break;
+              case "d":var tom4=new Audio("sounds/tom-4.mp3");
+                     tom4.play();break;
+              case "j":var snare=new Audio("sounds/snare.mp3");
+                     snare.play();break;
+              case "k":var crash=new Audio("sounds/crash.mp3");
+                     crash.play();break;
+              case "l":var kick=new Audio("sounds/kick-bass.mp3");
+                     kick.play();break;
+              default:console.log(but);
+         }
+}
+function animation(currentkey){
+    document.querySelector("."+currentkey).classList.add("pressed");
+    setTimeout(function(){
+       document.querySelector("."+currentkey).classList.remove("pressed");
+    },100);
+}
+setInterval(function(){
+document.querySelector(".time").innerHTML=new Date();
+},1000);
